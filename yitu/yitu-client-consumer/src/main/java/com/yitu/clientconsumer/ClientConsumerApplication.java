b3f087510ee309722c178a9cb9113ed60469afc4
marx828 <113021560@qq.com>
2018-05-08 17:39:32 +0800

    创建“我的易途”网站架构代码
    创建“我的易途”网站架构代码

---
+++ yitu/yitu-client-consumer/src/main/java/com/yitu/clientconsumer/ClientConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.yitu.clientconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableFeignClients
+public class ClientConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientConsumerApplication.class, args);
+    }
+}
